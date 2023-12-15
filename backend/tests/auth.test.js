1ef136f507d90648c4d6f33dd12a0fa66e69ea63
vamsikrishna <80871817+vamsikrishna1007@users.noreply.github.com>
2023-12-15 16:38:17 +0530

    added new test

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -30,8 +30,8 @@ describe('Registration and Login API Tests', () => {
   describe('Registration API', () => {
     it('should register a new user', async () => {
       const res = await request.post('/api/v1/user/register').send({
-        name: 'prdjahjitchag1',
-        email: 'spdfjhjkljhkldd.dunki@example.com',
+        name: 'prdfucvkjkdjitchag1',
+        email: 'spdfjhjkffdjfkjljhkldd.dunki@example.com',
         password: 'password123',
       });
 
